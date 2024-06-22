fe949baa9fc40e15d589e4df4c651652dbb33a81
Davi Ramon <davi.ramon@gmail.com>
2024-06-22 10:11:24 -0300

    feat: add find by id and create methods and interface imessage

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { MessagesService } from './messages.service';
+import { IMessage } from './IMessage';
 
 @Controller('messages')
 export class MessagesController {
@@ -8,5 +9,15 @@ export class MessagesController {
   @Get()
   findAll() {
     return this.messagesService.findAll();
+  }
+
+  @Get(':id')
+  findById(@Param() params) {
+    return this.messagesService.findById(+params.id);
+  }
+
+  @Post()
+  create(@Body() message: IMessage) {
+    return this.messagesService.create(message);
   }
 }
--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,39 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { IMessage } from './IMessage';
 
 @Injectable()
 export class MessagesService {
-  private messages = [
+  private messages: IMessage[] = [
     {
       id: 1,
-      nome: 'Davi ',
-    },
-    {
-      id: 2,
-      nome: 'Bruna',
-    },
-    {
-      id: 3,
       nome: 'Betoven',
     },
     {
-      id: 4,
+      id: 2,
       nome: 'Theodoro',
     },
     {
-      id: 5,
+      id: 3,
       nome: 'Ariel',
     },
     {
-      id: 6,
+      id: 4,
       nome: 'Kiwi',
     },
     {
-      id: 7,
+      id: 5,
       nome: 'Jasmão',
     },
   ];
 
   findAll() {
     return this.messages;
+  }
+
+  findById(id: number) {
+    return this.messages.find((message) => message.id === id);
+  }
+
+  create(message: IMessage) {
+    return this.messages.push(message);
   }
 }
